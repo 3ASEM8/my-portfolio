6c6f819fa4c75d104694af1e1ea19329d63910e2
assem <gddfggggdadg12@gmail.com>
2024-07-18 00:37:50 +0300

    fix image

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -10,16 +10,14 @@ const ProjectCard: FC<ProjectCardProps> = ({ imageUrl, title }) => {
   return (
     <article className="relative group border rounded-md shadow cursor-pointer overflow-hidden">
       <figure className="absolute size-full top-0 left-0 bg-gradient-to-t from-background/70 to-transparent" />
-      <Image
-        src={imageUrl}
-        width={200}
-        height={220}
-        alt={title}
-        layout="responsive" // يمكن استخدام 'fixed', 'intrinsic', 'responsive', 'fill'
-        quality={100} // تحسين الجودة إذا لزم الأمر
-        style={{ objectFit: "cover" }} // ضبط عرض الصورة
-        className="size-full rounded-sm group-hover:scale-105 transition"
-      />
+      <div className="w-[300px] h-[300px]">
+        <Image
+          src={imageUrl}
+          alt={title}
+          fill
+          className="size-full rounded-sm group-hover:scale-105 transition"
+        />
+      </div>
       <h3 className="absolute bottom-2 start-2 text-foreground group-hover:text-primary capitalize truncate font-semibold">
         {title}
       </h3>
